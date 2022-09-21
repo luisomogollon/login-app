52fbf1d4cc41398f948991710b014bc56013a6d1
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-09-21 18:27:27 -0400

    login alfa

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-    return (
-        <div>
-            <section class="h-full gradient-form bg-gray-50 md:h-screen"> </section>
+  return (
+    <div>
+  <section class="h-full gradient-form bg-gray-100 md:h-screen">
+  <div class="container py-12 px-6 h-full">
+    <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+      <div class="xl:w-10/12">
+        <div class="block bg-white shadow-lg rounded-lg">
+          <div class="lg:flex lg:flex-wrap g-0">
+            <div class="lg:w-6/12 px-4 md:px-0">
+              <div class="md:p-12 md:mx-6">
+                <div class="text-center">
+                 
+                  <h4 class="text-xl font-semibold mt-1 mb-12 pb-1"> Art</h4>
+                </div>
+                <form>
+                  
+                  <div class="mb-4">
+                    <input
+                      type="text"
+                      class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                      id="exampleFormControlInput1"
+                      placeholder="Username"
+                    />
+                  </div>
+                  <div class="mb-4">
+                    <input
+                      type="password"
+                      class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                      id="exampleFormControlInput1"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div class="text-center pt-1 mb-12 pb-1">
+                   
+                    <a class="text-gray-500" href="#!">Forgot password?</a>
+                  </div>
+                  <div class="flex items-center justify-between pb-6">
+                    <p class="mb-0 mr-2">Don't have an account?</p>
+                    
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div
+              class="lg:w-6/12 flex items-center bg-indigo-600 lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none"
+          
+            >
+              <div class="text-white px-4 py-6 md:p-12 md:mx-6">
+                <h4 class="text-xl font-semibold mb-6"></h4>
+                <p class="text-sm">
+                
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
+  );
 };
 
 export default Login;
