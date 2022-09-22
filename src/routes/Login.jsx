91d1f885bb72fde0cf154ff335c479ef23f3912c
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-09-21 20:58:53 -0400

    cambio 8:57pm

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -3,51 +3,46 @@ import React from "react";
 const Login = () => {
   return (
     <section class="w-full bg-white">
-<div class="mx-auto max-w-7xl">
-<div class="flex flex-col lg:flex-row">
-
-
-
-      <div class="w-full bg-white lg:w-6/12 xl:w-5/12">
-        <div class="flex flex-col items-start justify-start w-full h-full p-10 lg:p-16 xl:p-24">
-          <h4 class="w-full text-3xl font-bold">Login to Your Account</h4>
-
-          <p class="text-lg text-gray-500 mt-5 "> Login using social networks</p>
-          <div class="relative w-full mt-10 space-y-8">
-            <div class="relative">
-
-              <label class="font-medium text-gray-900">Email</label>
-              <input
-                type="text"
-                class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
-                placeholder="Enter Your Email Address"
-              />
-            </div>
-
-            <div class="relative">
-              <label class="font-medium text-gray-900">Password</label>
-              <input
-                type="password"
-                class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
-                placeholder="Password"
-              />
-            </div>
-
-            <div class="relative">
-              <a
-                href="#_"
-                class="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease"
-              >
-                Sign In
-              </a>
-            </div>
+      <div class="mx-auto max-w-7xl">
+        <div class="flex flex-col lg:flex-row">
+          <div class="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-gradient-to-r from-white via-white to-gray-100">
+            
           </div>
 
+          <div class="w-full bg-white lg:w-7/12 xl:w-5/12">
+            <div class="flex flex-col items-start justify-start w-full h-full p-10 lg:p-16 xl:p-24">
+              <h4 class="w-full text-3xl font-bold">Login to your Accout</h4>
+              <p class="text-lg text-gray-500">Login using social networks</p>
+              <div class="relative w-full mt-10 space-y-8">
+                <div class="relative">
+                  <label class="font-medium text-gray-900">Email</label>
+                  <input
+                    type="text"
+                    class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
+                    placeholder="Enter Your Email Address"
+                  />
+                </div>
+                <div class="relative">
+                  <label class="font-medium text-gray-900">Password</label>
+                  <input
+                    type="password"
+                    class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
+                    placeholder="Password"
+                  />
+                </div>
+                <div class="relative">
+                  <a
+                    href="#_"
+                    class="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease"
+                  >
+                    Create Account
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-</div>
-</div>
-      
     </section>
   );
 };
