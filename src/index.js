09ad77edd74a94a29cee017a825f6c3c4ce0139e
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-09-22 16:11:18 -0400

    cambios 4:11

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./routes/Login.jsx";
 import Create from "./routes/Create.jsx";
 
-//import { Layout } from "./Layout";
+import { Layout } from "./Layout";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,9 +13,10 @@ root.render(
   
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}>
-          <Route index element={<Create />} />
-          <Route path="create" element={<Create />} />
+        <Route path="/" element={< Layout/>}>
+        <Route index element={<Login />} />
+          <Route path="Create" element={<Create />} />
+          
         </Route>
       </Routes>
     </BrowserRouter>
